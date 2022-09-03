8cbf9817b7ac3a3f00922fabede2b2b29834539f
Yoyo Yang <yoyo.truly@gmail.com>
2022-09-02 21:22:43 -0400

    Add category and product sum to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @categories = Category.order(id: :desc).all
+    @products = Product.order(id: :desc).all
   end
 end
